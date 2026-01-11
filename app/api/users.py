ba1753bfbe5cae756700cbabbf22dd1ad444c9d7
icodexin <codexin@foxmail.com>
2026-01-11 22:23:10 +0800

    fix: 更新参数注解，以便在SwaggerUI中正确显示schema

--- app/api/users.py
+++ app/api/users.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core import get_db
+from app.schemas.user import UserCreate
 from app.services.user import create_user
 
 router = APIRouter(prefix="/users")
 
 @router.post("/", response_model=dict, status_code=status.HTTP_201_CREATED)
-async def create_user_endpoint(new_user: dict, db: AsyncSession = Depends(get_db)):
+async def create_user_endpoint(new_user: UserCreate, db: AsyncSession = Depends(get_db)):
     """创建新用户"""
     try:
         created_user = await create_user(new_user, db)
